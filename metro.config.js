4376acdcc90ffe4afe8750e6de723cc0f9e9f6f2
NilsLiebrand <108192145+NilsLiebrand@users.noreply.github.com>
2022-11-13 20:01:31 +0100

    upload

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,8 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-defaultConfig.resolver.assetExts.push('db');
-
-module.exports = getDefaultConfig(__dirname);
-
+module.exports = config;
 
 
